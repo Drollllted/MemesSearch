9c22ca3cc6c3c97daab419e8cf9c57482b72aa5e
Artem Tarasov <artemdrolllted@gmail.com>
2024-06-03 19:23:04 +0300

    Initial Commit

---
+++ MemesSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemesSearch
+//
+//  Created by Drolllted on 03.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
